d481cd62efa2631ff216979d1fd7e0e70fb483af
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-01-21 15:11:13 -0500

    Recieve and print serial data from arduino

--- server/server.py
+++ server/server.py
@@ -54,7 +54,6 @@ class serial_port_read:
         self.notes = [41, 44, 48, 49, 51, 53, 56]
         self.note_status = [False, False, False, False, False, False, False]
 
-
         print('Trying to connect to: ' + str(port_name) + ' at ' + str(baud_rate) + ' BAUD.')
         try:
             self.serial_connection = serial.Serial(port_name, baud_rate, timeout=4)
@@ -62,6 +61,7 @@ class serial_port_read:
         except:
             print("Failed to connect with " + str(port_name) + ' at ' + str(baud_rate) + ' BAUD.')
             sys.exit(1)
+
 
     def start_serial_reading(self):
         if self.thread == None:
@@ -96,81 +96,81 @@ class serial_port_read:
             data_value = static_data[(data_length_cumsum[i]):(data_length_cumsum[i+1])]
             value,  = struct.unpack(self.data_types[i], data_value)
             self.data[i].append(value)    # we get the latest data point and append it to our deque in the data array
-            # print(value,  end="\t")
-        # print("\r",end="")
+            print(value,  end="\t")
+        print("\r",end="")
 
-        self.radius = np.asarray(self.data[0])
-        self.angle = np.asarray(self.data[1])*np.pi/180
+        # self.radius = np.asarray(self.data[0])
+        # self.angle = np.asarray(self.data[1])*np.pi/180
 
-        if (figNumber == 1):
+        # if (figNumber == 1):
 
-            plt.figure(fig.number)
-            ax.clear()            
+        #     plt.figure(fig.number)
+        #     ax.clear()            
 
-            # define binning: 0m to 4m with steps of 12.5cm (32 steps)
-            self.rbins = np.linspace(0,400, 40)
-            self.abins = np.linspace(-0.1,2*np.pi-0.1, 40)
+        #     # define binning: 0m to 4m with steps of 12.5cm (32 steps)
+        #     self.rbins = np.linspace(0,400, 40)
+        #     self.abins = np.linspace(-0.1,2*np.pi-0.1, 40)
 
-            self.hist, _ , _ = np.histogram2d(self.angle, self.radius, bins=(self.abins, self.rbins), density=True)
-            self.A, self.R = np.meshgrid(self.abins, self.rbins)
+        #     self.hist, _ , _ = np.histogram2d(self.angle, self.radius, bins=(self.abins, self.rbins), density=True)
+        #     self.A, self.R = np.meshgrid(self.abins, self.rbins)
 
-            self.pc = ax.pcolormesh(self.A, self.R, self.hist.T, cmap="magma")
+        #     self.pc = ax.pcolormesh(self.A, self.R, self.hist.T, cmap="magma")
             
-            ax.set_rmax(400)
-            ax.set_rorigin(20)
+        #     ax.set_rmax(400)
+        #     ax.set_rorigin(20)
 
-        if (figNumber == 2):
+        # if (figNumber == 2):
 
-            plt.figure(fig.number)
+        #     plt.figure(fig.number)
              
-            ax[0].clear()
-            ax[1].clear()
+        #     ax[0].clear()
+        #     ax[1].clear()
 
-            # self.weights_radius = np.ones_like(self.radius)/maxDataLength
-            self.weights_radius = np.ones_like(self.radius)/np.max(self.radius)
+        #     # self.weights_radius = np.ones_like(self.radius)/maxDataLength
+        #     self.weights_radius = np.ones_like(self.radius)/np.max(self.radius)
 
-            self.N_azimuth, self.bins_azimut, self.patches_azimuth = ax[0].hist(self.data[1],bins=range(-4,365-4,9))
-            self.N_radius, self.bins_radius, self.patches_radius = ax[1].hist(self.radius,bins=np.linspace(20,300,8), weights=self.weights_radius)
-            ax[1].set_ylim(0,1)
+        #     self.N_azimuth, self.bins_azimut, self.patches_azimuth = ax[0].hist(self.data[1],bins=range(-4,365-4,9))
+        #     self.N_radius, self.bins_radius, self.patches_radius = ax[1].hist(self.radius,bins=np.linspace(20,300,8), weights=self.weights_radius)
+        #     ax[1].set_ylim(0,1)
 
 
-            # We'll color code by height, but you could use any scalar
-            self.fracs = self.N_radius
+        #     # We'll color code by height, but you could use any scalar
+        #     self.fracs = self.N_radius
 
-            # we need to normalize the data to 0..1 for the full range of the colormap
-            self.norm = colors.Normalize(self.fracs.min(), self.fracs.max())
+        #     # we need to normalize the data to 0..1 for the full range of the colormap
+        #     self.norm = colors.Normalize(self.fracs.min(), self.fracs.max())
 
-            # Now, we'll loop through our objects and set the color of each accordingly
-            for thisfrac, thispatch in zip(self.fracs, self.patches_radius):
-                color = plt.cm.gist_yarg(self.norm(thisfrac))
-                thispatch.set_facecolor(color)
+        #     # Now, we'll loop through our objects and set the color of each accordingly
+        #     for thisfrac, thispatch in zip(self.fracs, self.patches_radius):
+        #         color = plt.cm.gist_yarg(self.norm(thisfrac))
+        #         thispatch.set_facecolor(color)
 
             
 
-            for i in range(0,np.shape(self.fracs)[0]):
+        #     for i in range(0,np.shape(self.fracs)[0]):
 
-                if (self.fracs[i] > 0.00001 ):
+        #         if (self.fracs[i] > 0.00001 ):
                     
-                    if (self.note_status[i] == False):
+        #             if (self.note_status[i] == False):
 
-                        self.midi_msg = mido.Message('note_on', note=self.notes[i], channel=i)
-                        self.midi_outport.send(self.midi_msg)
-                        print("Note on", self.notes[i])
-                        self.note_status[i] = True
+        #                 self.midi_msg = mido.Message('note_on', note=self.notes[i], channel=i)
+        #                 self.midi_outport.send(self.midi_msg)
+        #                 print("Note on", self.notes[i])
+        #                 self.note_status[i] = True
 
-                    # self.midi_msg = mido.Message('control_change', channel=i, control=0, value=int(self.N_radius[i]*127), time=0)
-                    self.midi_msg = mido.Message('control_change', channel=i, control=0, value=int(127), time=0)
-                    # self.midi_outport.send(self.midi_msg)
-                    # print('CC channel',i+1,'value',int(self.N_radius[i]*127))
+        #             # self.midi_msg = mido.Message('control_change', channel=i, control=0, value=int(self.N_radius[i]*127), time=0)
+        #             self.midi_msg = mido.Message('control_change', channel=i, control=0, value=int(127), time=0)
+        #             # self.midi_outport.send(self.midi_msg)
+        #             # print('CC channel',i+1,'value',int(self.N_radius[i]*127))
 
-                elif (self.fracs[i] < 0.00001 ):
+        #         elif (self.fracs[i] < 0.00001 ):
 
-                    if (self.note_status[i] == True):
+        #             if (self.note_status[i] == True):
 
-                        self.midi_msg = mido.Message('note_off', note=self.notes[i], channel=i)
-                        self.midi_outport.send(self.midi_msg)
-                        # print("Note off", self.notes[i])
-                        self.note_status[i] = False
+        #                 self.midi_msg = mido.Message('note_off', note=self.notes[i], channel=i)
+        #                 self.midi_outport.send(self.midi_msg)
+        #                 # print("Note off", self.notes[i])
+        #                 self.note_status[i] = False
                 
     def close(self):
         self.is_run = False
@@ -198,7 +198,7 @@ def main():
     num_data_bytes = np.sum(data_length)    # number of bytes of one data stream
     
     s = serial_port_read(port_name, baud_rate, stack_size_data_points, num_data_bytes, data_types, data_length)   # initializes all required variables
-    s.start_serial_reading()                                               # starts background thread
+    s.start_serial_reading()                                               # starts reading serial data in the background thread
 
     # plotting starts below
     pltInterval = 50    # Period at which the plot animation updates [ms]
